12a82e90d2c7ea13f682c87ed46591cb60f47d4f
Deli <1765939870@qq.com>
2025-03-09 23:55:12 +0800

    Update UIMapNodeBase.cs

--- Assets/Scripts/MyEntity/UIMapNodeBase.cs
+++ Assets/Scripts/MyEntity/UIMapNodeBase.cs
@@ -6,19 +6,4 @@ using UnityEngine.UI;
 public class UIMapNodeBase : MonoBehaviour
 {
     public Button BtnEnter;
-
-    void Awake()
-    {
-        BtnEnter.onClick.AddListener(OnClickEnter);
-    }
-
-    void OnClickEnter()
-    {
-        MyEvent.Fire(new OnClickEnterMapNodeEvent(){UIMapNode = this});
-    }
-}
-
-public class OnClickEnterMapNodeEvent
-{
-    public UIMapNodeBase UIMapNode;
 }
