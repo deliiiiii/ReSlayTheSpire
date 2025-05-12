8a331f7e1081582c7b66ac2f7b44e7d168694688
Deli <1765939870@qq.com>
2025-05-12 20:50:58 +0800

    重构bind

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -3,39 +3,40 @@ using System.ComponentModel;
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
-#region AddINotifyPropertyChangedInterface
+// #region AddINotifyPropertyChangedInterface
 // public class Test : MonoBehaviour
 // {
 //     TestProperty testProperty = new TestProperty();
 //     void Update()
-//     {
-//         if (Input.GetKeyDown(KeyCode.A))
-//         {
-//             testProperty.IntProperty1++;
-//         }
+     // {
+     //     if (Input.GetKeyDown(KeyCode.A))
+     //     {
+     //         testProperty.IntProperty1++;
+     //     }
 //     }
 // }
-
-[AddINotifyPropertyChangedInterface]
-public class TestProperty
-{
-    // [AlsoNotifyFor(nameof(IntProperty2))]
-    public int IntProperty1 { get; set; }
-
-    [DoNotNotify]
-    public int IntProperty2 => IntProperty1 + 1;
-    void OnIntProperty1Changed()
-    {
-        Debug.Log($"[Fody-Callback] IntProperty1 -> {IntProperty1}");
-    }
-    void OnIntProperty2Changed()
-    {
-        Debug.Log($"[Fody-Callback] IntProperty2 -> {IntProperty2}");
-    }
-}
-
-#endregion
+//
+// [AddINotifyPropertyChangedInterface]
+// public class TestProperty
+// {
+//     // [AlsoNotifyFor(nameof(IntProperty2))]
+//     public int IntProperty1 { get; set; }
+//
+//     [DoNotNotify]
+//     public int IntProperty2 => IntProperty1 + 1;
+//     void OnIntProperty1Changed()
+//     {
+//         Debug.Log($"[Fody-Callback] IntProperty1 -> {IntProperty1}");
+//     }
+//     void OnIntProperty2Changed()
+//     {
+//         Debug.Log($"[Fody-Callback] IntProperty2 -> {IntProperty2}");
+//     }
+// }
+//
+// #endregion
 
 
 
@@ -53,16 +54,19 @@ public class Observable<T>
             {
                 return;
             }
-            onValueChanged?.Invoke(value);
+
+            onValueChangedBefore?.Invoke(_value);
             _value = value;
+            onValueChangedAfter?.Invoke(_value);
         }
     }
-    public event Action<T> onValueChanged;
+    public event Action<T> onValueChangedBefore;
+    public event Action<T> onValueChangedAfter;
 
     public Observable(T initValue, Action<T> initEvent = null)
     {
         _value = initValue;
-        onValueChanged += initEvent;
+        onValueChangedAfter += initEvent;
         Dic = new Dictionary<object, Action<T>>();
     }
 
@@ -71,41 +75,76 @@ public class Observable<T>
 }
 
 
-public static class BindStaticClass
+public static class Binder
 {
+    //
+    // public static void Bind<T1, T2>(Observable<T1> from, Observable<T2> to, Func<T1, T2> converter)
+    // {
+    //     if (from.Dic.ContainsKey(to))
+    //     {
+    //         Debug.LogWarning($"Binding already exists for this source:{from} and target:{to}.");
+    //         Unbind(from, to);
+    //     }
+    //     Action<T1> ac = (newValue) =>
+    //     {
+    //         to.Value = converter(newValue);
+    //     };
+    //     from.onValueChanged += ac;
+    //     from.Dic[to] = ac;
+    // }
 
-    public static void Bind<T1, T2>(Observable<T1> from, Observable<T2> to, Func<T1, T2> converter)
+    
+    // public static void Bind<T1, T2>(Observable<T1> from,ref T2 to, Func<T1, T2> converter)
+    // {
+    //     if (from.Dic.ContainsKey(to))
+    //     {
+    //         Debug.LogWarning($"Binding already exists for this source:{from} and target:{to}.");
+    //         Unbind(from,ref to);
+    //     }
+    //     Action<T1> ac = (newValue) =>
+    //     {
+    //         to = converter(newValue);
+    //     };
+    //     from.onValueChanged += ac;
+    //     from.Dic[to] = ac;
+    // }
+    
+    
+    public static void BindAction<T1>(Observable<T1> from, Action action)
     {
-        if (from.Dic.ContainsKey(to))
+        from.onValueChangedAfter += (T1 t1) => action();
+    }
+
+    public static void BindText<T1>(Observable<T1> from, Text text)
+    {
+        from.onValueChangedAfter += (T1 t1) =>
         {
-            Debug.LogWarning($"Binding already exists for this source:{from} and target:{to}.");
-            Unbind(from, to);
-        }
-        Action<T1> ac = (newValue) =>
-        {
-            to.Value = converter(newValue);
+            text.text = from.Value.ToString();
         };
-        from.onValueChanged += ac;
-        from.Dic[to] = ac;
     }
-    public static void Unbind<T1, T2>(Observable<T1> from, Observable<T2> to)
-    {
-        if (from.Dic.TryGetValue(to, out var v))
-        {
-            from.onValueChanged -= v;
-            from.Dic.Remove(v);
-        }
 
-        
-        
-    }
+    
+    
+    // public static void Unbind<T1, T2>(Observable<T1> from, Observable<T2> to)
+    // {
+    //     if (from.Dic.TryGetValue(to, out var v))
+    //     {
+    //         from.onValueChanged -= v;
+    //         from.Dic.Remove(v);
+    //     }
+    // }
+    //
+    // public static void Unbind<T1, T2>(Observable<T1> from, ref T2 to)
+    // {
+    //     if (from.Dic.TryGetValue(to, out var v))
+    //     {
+    //         from.onValueChanged -= v;
+    //         from.Dic.Remove(v);
+    //     }
+    // }
 
 }
 
-public static class IntModifier
-{
-    public static Func<int, float> Func1p1 = (int v) => v * 1.1f;
-}
 
 [Serializable]
 public class Model1
@@ -113,40 +152,19 @@ public class Model1
     public Observable<int> I = new Observable<int>(66);
 }
 
-[Serializable]
-public class Model2
-{
-    public Observable<float> F = new Observable<float>(33.3f);
-}
 
 public class Test2 : MonoBehaviour
 {
     public Model1 Model1Instance = new Model1();
-    public Model2 Model2Instance = new Model2();
+    float f = 111f;
+    public Text TestText;
 
-
-    // public Observable<bool> Input;
+    void Awake()
+    {
+        Binder.BindText(Model1Instance.I, TestText);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Model1Instance.I.Value += 1;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            BindStaticClass
-                .Bind(Model1Instance.I, Model2Instance.F, IntModifier.Func1p1);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            BindStaticClass
-                .Unbind(Model1Instance.I, Model2Instance.F);
-        }
     }
 }
-
-// BindStaticClass.From(Model1Instance.I).To(Model2Instance.F).
-//
