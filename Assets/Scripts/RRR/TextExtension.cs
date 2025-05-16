5c19ada155a65f6c7a55e9f9d6fad9129f2a2d92
Deli <1765939870@qq.com>
2025-05-17 03:56:19 +0800

    feat：学会了Unitask+Cancel

--- Assets/Scripts/RRR/TextExtension.cs
+++ Assets/Scripts/RRR/TextExtension.cs
@@ -1,8 +1,12 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 public class TextExtensionData
@@ -12,15 +16,34 @@ public class TextExtensionData
     public float TarValue;
     public float DeltaPerSecond;
     public string Format;
+    public CancellationTokenSource CancelTokenSource;
 }
 
 public static class TextExtension
 {
     static Dictionary<Text, TextExtensionData> dic = new();
-    const float UPDATE_INTERVAL = 0.02f;
+    static int staticCount = 0;
+    const float UPDATE_INTERVAL = 0.02f; 
     public static void DoFluent(this Text txt, float tarValue, float deltaPerSecond, string format = "")
     {
         
+#if UNITY_EDITOR
+        if (staticCount == 0)
+        {
+            staticCount++;
+            EditorApplication.playModeStateChanged += (s) =>
+            {
+                if (s != PlayModeStateChange.ExitingPlayMode)
+                    return;
+                MyDebug.LogError("Cancel");
+                foreach (var it in dic.Keys)
+                {
+                    dic[it].CancelTokenSource.Cancel();
+                }
+                dic.Clear();
+            };
+        }
+#endif
         if (!dic.ContainsKey(txt))
         {
             dic.Add(txt, new TextExtensionData
@@ -28,6 +51,7 @@ public static class TextExtension
                 CurValue = float.TryParse(txt.text, out float cur) ? cur : 0f,
                 DeltaPerSecond = deltaPerSecond,
                 Format = format,
+                CancelTokenSource = new CancellationTokenSource(),
             });
         }
         dic[txt].TarValue = tarValue;
@@ -56,7 +80,7 @@ public static class TextExtension
                     dic[txt].CurValue
                     );
             txt.text = dic[txt].CurValue.ToString(dic[txt].Format);
-            await UniTask.WaitForSeconds(UPDATE_INTERVAL);
+            await UniTask.WaitForSeconds(UPDATE_INTERVAL, cancellationToken: dic[txt].CancelTokenSource.Token);
         }
         txt.text = dic[txt].TarValue.ToString(dic[txt].Format);
         dic.Remove(txt);
