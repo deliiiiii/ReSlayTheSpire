2738318ab4b6014ae878f5623310fbc4570e0c1b
Deli <1765939870@qq.com>
2025-07-15 20:15:56 +0800

    fixBoxModelManager

--- Assets/Scripts/Violee/Model/BoxModel.cs
+++ Assets/Scripts/Violee/Model/BoxModel.cs
@@ -1,59 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Sirenix.OdinInspector;
-using UnityEditor.Callbacks;
+﻿using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Violee
 {
-    public class BoxModel : Singleton<BoxModel>
+    public class BoxModel : MonoBehaviour
     {
-        protected override void Awake()
-        {
-            base.Awake();
-            boxParent = new GameObject("BoxParent").transform;
-            MapModel.OnAddBox += SpawnBox;
-            MapModel.OnRemoveBox += DestroyBox;
-            MapModel.OnInputEnd += ShowSprite;
-        }
-        
-        [ShowInInspector]
-        BoxData boxData;
-        Dictionary<Loc, BoxModel> boxModelDic = new ();
-        Transform boxParent;
-
-        
-        #region Event
-        void SpawnBox(Loc loc, BoxData fBoxData)
-        {
-            // TODO 对象池
-            var boxGO = new GameObject($"Box {loc.X} {loc.Y}");
-            boxGO.transform.SetParent(boxParent);
-            boxGO.transform.position = new Vector3(loc.X, loc.Y, 0);
-            
-            var boxRenderer = boxGO.AddComponent<SpriteRenderer>();
-            boxRenderer.sprite = fBoxData.Sprite;
-            boxRenderer.enabled = SettingsModel.SettingsConfig.ShowBoxWhenCreated;
-            
-            var boxModel = boxGO.AddComponent<BoxModel>();
-            boxModel.boxData = fBoxData;
-            
-            boxModelDic.Add(loc, boxModel);
-        }
-        
-        void DestroyBox(Loc loc)
-        {
-            // TODO 对象池
-            Destroy(boxModelDic[loc].gameObject);
-            boxModelDic.Remove(loc);
-        }
-        void ShowSprite(Loc loc)
-        {
-            boxModelDic[loc].GetComponent<SpriteRenderer>().enabled = true;
-        }
-        #endregion
+        public BoxData BoxData;
     }
 }
---
+++ Assets/Scripts/Violee/Model/BoxModelManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Violee
+{
+    public class BoxModelManager : MonoBehaviour
+    {
+        void Awake()
+        {
+            MapModel.OnAddBox += SpawnBox;
+            MapModel.OnRemoveBox += DestroyBox;
+            MapModel.OnInputEnd += ShowSprite;
+        }
+        Dictionary<Loc, BoxModel> boxModelDic = new ();
+        #region Event
+        void SpawnBox(Loc loc, BoxData fBoxData)
+        {
+            // TODO 对象池
+            var boxGO = new GameObject($"Box {loc.X} {loc.Y}");
+            boxGO.transform.SetParent(transform);
+            boxGO.transform.position = new Vector3(loc.X, loc.Y, 0);
+            
+            var boxRenderer = boxGO.AddComponent<SpriteRenderer>();
+            boxRenderer.sprite = fBoxData.Sprite;
+            boxRenderer.enabled = SettingsModel.SettingsConfig.ShowBoxWhenCreated;
+            
+            var boxModel = boxGO.AddComponent<BoxModel>();
+            boxModel.BoxData = fBoxData;
+            
+            boxModelDic.Add(loc, boxModel);
+        }
+        
+        void DestroyBox(Loc loc)
+        {
+            // TODO 对象池
+            Destroy(boxModelDic[loc].gameObject);
+            boxModelDic.Remove(loc);
+        }
+        void ShowSprite(Loc loc)
+        {
+            boxModelDic[loc].GetComponent<SpriteRenderer>().enabled = true;
+        }
+        #endregion
+    }
+}
