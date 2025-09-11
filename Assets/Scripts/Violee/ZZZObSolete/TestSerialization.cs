84cdaa4d25fa71afe866164d7d2b461539aa0c28
Deli <1765939870@qq.com>
2025-09-11 21:16:50 +0800

    Update TestSerialization.cs

--- Assets/Scripts/Violee/ZZZObSolete/TestSerialization.cs
+++ Assets/Scripts/Violee/ZZZObSolete/TestSerialization.cs
@@ -10,5 +10,26 @@ public class TestSerialization : SerializedMonoBehaviour
 {
     [OdinSerialize][NonSerialized]
     public required TestS TestS;
+    
+    void Awake()
+    {
+        (new TestA() as ITest).Func();
+        // ITest.Func();
+    }
 }
 
+interface ITest
+{
+    virtual void Func()
+    {
+        Debug.Log("ITest");
+    }
+}
+
+class TestA : ITest
+{
+    public void Func()
+    {
+        Debug.Log("TestA");
+    }
+}
