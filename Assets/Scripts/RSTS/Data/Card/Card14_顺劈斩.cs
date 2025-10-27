3b65139f87cbc3f774e7548c6922dabf78b94d53
Deli <1765939870@qq.com>
2025-10-27 15:12:21 +0800

    feat：Card14 顺劈斩

---
+++ Assets/Scripts/RSTS/Data/Card/Card14_顺劈斩.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace RSTS;
+[CardID(14)][Serializable]
+public class Card14 : CardDataBase
+{
+    int atk => EmbedInt(0);
+    public override UniTask YieldAsync(BothTurnData bothTurnData, int costEnergy)
+    {
+        bothTurnData.AttackAllEnemies(atk);
+        return UniTask.CompletedTask;
+    }
+}
