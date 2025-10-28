e7bdb3e8b4c02d17f37134d0484e2bf093f5fd23
Deli <1765939870@qq.com>
2025-10-28 14:38:05 +0800

    feat：Card21 灼热攻击

---
+++ Assets/Scripts/RSTS/Data/Card/Card21_灼热攻击.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace RSTS;
+[CardID(21)][Serializable]
+public class Card21 : CardDataBase
+{
+    int atk => NthEmbedAs<EmbedAttack>(0).AttackValue;
+
+    public override UniTask YieldAsync(BothTurnData bothTurnData, int costEnergy)
+    {
+        bothTurnData.AttackEnemy(Target, atk);
+        return UniTask.CompletedTask;
+    }
+}
