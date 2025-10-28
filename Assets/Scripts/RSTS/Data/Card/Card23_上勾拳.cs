b603fa24fced768ffa1007a1d7613f0728b357da
Deli <1765939870@qq.com>
2025-10-28 14:53:48 +0800

    feat： Card23 上勾拳

---
+++ Assets/Scripts/RSTS/Data/Card/Card23_上勾拳.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace RSTS;
+[CardID(23)][Serializable]
+public class Card23 : CardDataBase
+{
+    int atk => NthEmbedAs<EmbedAttack>(0).AttackValue;
+    BuffDataWeak buffWeak => NthEmbedAsBuffCopy<BuffDataWeak>(1);
+    BuffDataVulnerable buffVulnerable => NthEmbedAsBuffCopy<BuffDataVulnerable>(2);
+
+    public override UniTask YieldAsync(BothTurnData bothTurnData, int costEnergy)
+    {
+        bothTurnData.AttackEnemy(Target, atk);
+        bothTurnData.AddBuffToEnemy(Target, buffWeak);
+        bothTurnData.AddBuffToEnemy(Target, buffVulnerable);
+        return UniTask.CompletedTask;
+    }
+}
