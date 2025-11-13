f8423a7d21f25ebcc278a5a703ab1d6942155f90
Deli <1765939870@qq.com>
2025-11-13 22:31:12 +0800

    feat：Observable类的自定义drawer

---
+++ Assets/Scripts/RSTS/Editor/FieldDrawer/ObservableDrawer.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Sirenix.OdinInspector.Editor;
+using UnityEngine;
+
+namespace RSTS.Editor
+{
+    public class ObservableDrawer<T> : OdinValueDrawer<Observable<T>> where T : struct
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            var valueChild = Property.Children.FirstOrDefault(c => c.Name == "value");
+            if (valueChild != null)
+            {
+                valueChild.Draw(label);
+            }
+            else
+            {
+                CallNextDrawer(label);
+            }
+        }
+    }
+}
